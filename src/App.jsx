c5d538d1ede761c26100b5956f5c4a196dc47c30
mwendwamark <mwendwamark310@gmail.com>
2024-09-04 07:09:31 +0300

    File setup and organization

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h2>HELOO my gEE</h2>
+    
+  <p>HElolo hoiede kdxfjasd  ksdmark mmwendwa</p>
+    </>
+  )
+}
+
+export default App
