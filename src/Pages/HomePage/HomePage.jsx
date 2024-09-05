0ce683998393fa07345a709f1552c8821cbbee72
mwendwamark <mwendwamark310@gmail.com>
2024-09-05 13:49:18 +0300

    create the introduction section

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -3,12 +3,13 @@ import { NavLink } from "react-router-dom";
 import "./HomePage.css";
 import landlordimg from "../../assets/Hero-landlord.png";
 import studentimg from "../../assets/Hero-student.png";
+import introImg from "../../assets/Home-intro-img.png";
 
 const HomePage = () => {
   return (
     <>
-      <div className="home-page container">
-        <div className="home-hero-section">
+      <div className="home-page">
+        <div className="home-hero-section container">
           <div className="hero-title">
             <h2>Jamii hostels</h2>
             <h1>Streamline your hostel hunting experience</h1>{" "}
@@ -44,6 +45,38 @@ const HomePage = () => {
               </div>{" "}
               <div className="landlord-img">
                 <img src={landlordimg} alt="landlord-image" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="home-introduction-section">
+          <div className="intro-image-contents container">
+            <div className="intro-image">
+              <img src={introImg} alt="" />
+            </div>
+            <div className="intro-contents">
+              <h1>Introducing Jamii hostels</h1>
+              <h2>Find your perfect home away from home</h2>
+              <p>
+                Jamii Hostels is the ultimate platform for students seeking
+                comfortable and affordable accommodation near their
+                universities. We bridge the gap between students and hostel
+                owners, providing a seamless and efficient booking experience.
+              </p>
+              <div className="intro-section-data">
+                <div className="data-item">
+                  <h2 className="data-number">8475+</h2>
+                  <p className="data-label">Students</p>
+                </div>
+                <div className="data-item">
+                  <h2 className="data-number">594+</h2>
+                  <p className="data-label">Landlords hosted</p>
+                </div>
+                <div className="data-item">
+                  <h2 className="data-number">12065+</h2>
+                  <p className="data-label">Hostels listed</p>
+                </div>
               </div>
             </div>
           </div>
