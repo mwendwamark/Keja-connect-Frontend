ce6a2aba14278a2a23d13ac8ec2d523544244ea8
mwendwamark <mwendwamark310@gmail.com>
2024-09-12 08:02:03 +0300

    style the login pages and make them responsive

--- src/Landlords/Login/LandlordLogin.jsx
+++ src/Landlords/Login/LandlordLogin.jsx
@@ -1,89 +1,8 @@
-// import React, { useState, useEffect } from "react";
-// import "./LandlordLogin.css";
-// import { FaEye, FaEyeSlash } from "react-icons/fa";
-// import { useNavigate } from "react-router-dom";
-
-// function LandlordLogin() {
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [errors, setErrors] = useState([]);
-//   const [showPassword, setShowPassword] = useState(false);
-//   const navigate = useNavigate();
-
-//    function handleSubmit(e) {
-//      e.preventDefault();
-
-//      fetch("http://localhost:3000/landlord-login", {
-//        method: "POST",
-//        headers: { "Content-Type": "application/json" },
-//        body: JSON.stringify({ email, password }),
-//      }).then((r) => {
-//        if (r.ok) {
-//          r.json().then((user) => {
-//            localStorage.setItem("user", JSON.stringify(user));
-//            navigate("/landlord-dashboard");
-//          });
-//        } else {
-//          r.json().then((e) => setErrors([e.error]));
-//        }
-//      });
-//    }
-
-//   return (
-//     <div className="landlord-login-page">
-//       <div className="landlord-login-container">
-//         <h1 className="login-heading">Landlord Login</h1>
-//         <form className="landlord-login-form" onSubmit={handleSubmit}>
-//           <label htmlFor="email">Email</label>
-//           <input
-//             type="email"
-//             id="email"
-//             value={email}
-//             onChange={(e) => setEmail(e.target.value)}
-//             required
-//           />
-
-//           <label htmlFor="password">Password</label>
-//           <div className="password-field">
-//             <input
-//               type={showPassword ? "text" : "password"}
-//               id="password"
-//               value={password}
-//               onChange={(e) => setPassword(e.target.value)}
-//               required
-//             />
-//             <button
-//               type="button"
-//               onClick={() => setShowPassword(!showPassword)}
-//               className="eye-icon"
-//             >
-//               {showPassword ? <FaEyeSlash /> : <FaEye />}
-//             </button>
-//           </div>
-
-//           <div className="error-messages">
-//             {errors.map((error, index) => (
-//               <p key={index} style={{ color: "red" }}>
-//                 {error}
-//               </p>
-//             ))}
-//           </div>
-
-//           <button type="submit" className="login-btn">
-//             Login
-//           </button>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default LandlordLogin;
-
 import React, { useState, useEffect } from "react";
 import "./LandlordLogin.css";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, NavLink } from "react-router-dom";
+import img from "../../assets/landlordlogin.png";
 
 function LandlordLogin() {
   const [email, setEmail] = useState("");
@@ -116,41 +35,54 @@ function LandlordLogin() {
           navigate("/landlord-dashboard");
         });
       } else {
-        r.json().then((e) => setErrors([e.error])); // Assuming error is a string
+        r.json().then((e) => setErrors([e.error])); // Make the error messages to be an array
       }
     });
   }
 
   return (
-    <div className="landlord-login-page">
-      <div className="landlord-login-container">
-        <h1 className="login-heading">Landlord Login</h1>
-        <form className="landlord-login-form" onSubmit={handleSubmit}>
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
+    <div className="landlord-login-page container">
+      <div className="landlord-login-container container">
+        <div className="landlord-login-image">
+          <img src={img} alt="landlord" />
+        </div>
 
-          <label htmlFor="password">Password</label>
-          <div className="password-field">
+        <form className="landlord-login-form" onSubmit={handleSubmit}>
+          <div className="landlord-login-form-title">
+            <h1 className="h2-heading">Welcome back!</h1>
+            <p>Login</p>
+          </div>
+          <div className="landlord-login-input">
+            <label htmlFor="email">Email</label>
             <input
-              type={showPassword ? "text" : "password"}
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               required
             />
-            <button
-              type="button"
-              onClick={() => setShowPassword(!showPassword)}
-              className="eye-icon"
-            >
-              {showPassword ? <FaEyeSlash /> : <FaEye />}
-            </button>
+          </div>
+          <div className="landlord-login-password">
+            <label htmlFor="password">Password</label>
+            <div className="landlord-login-password-field">
+              <input
+                type={showPassword ? "text" : "password"}
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className="landlord-login-eye-icon"
+              >
+                {showPassword ? <FaEyeSlash /> : <FaEye />}
+              </button>
+            </div>
+          </div>
+          <div className="landlord-forgot-pass">
+            <NavLink to="/forgot-password">Forgot password?</NavLink>
           </div>
 
           <div className="error-messages">
@@ -161,9 +93,15 @@ function LandlordLogin() {
             ))}
           </div>
 
-          <button type="submit" className="login-btn">
+          <button type="submit" className="landlord-login-btn">
             Login
           </button>
+          <div className="landlord-dont-have-account">
+            <p>
+              Don't have an account?
+              <NavLink to="/landlord-signup">Signup</NavLink>
+            </p>
+          </div>
         </form>
       </div>
     </div>
@@ -171,4 +109,3 @@ function LandlordLogin() {
 }
 
 export default LandlordLogin;
-
--- src/Students/Login/StudentLogin.jsx
+++ src/Students/Login/StudentLogin.jsx
@@ -1,89 +1,8 @@
-// import React, { useState } from "react";
-// import { useNavigate } from "react-router-dom";
-// import "./StudentLogin.css";
-// import { FaEye, FaEyeSlash } from "react-icons/fa";
-
-// const StudentLogin = () => {
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [showPassword, setShowPassword] = useState(false);
-//   const [errors, setErrors] = useState([]);
-//   const navigate = useNavigate();
-
-//   function handleSubmit(e) {
-//     e.preventDefault();
-
-//     fetch("http://localhost:3000/student-login", {
-//       method: "POST",
-//       headers: { "Content-Type": "application/json" },
-//       body: JSON.stringify({ email, password }),
-//     }).then((r) => {
-//       if (r.ok) {
-//         r.json().then((user) => {
-//           localStorage.setItem("user", JSON.stringify(user));
-//           navigate("/hostels");
-//         });
-//       } else {
-//         r.json().then((e) => setErrors([e.error])); // Update here
-//       }
-//     });
-//   }
-
-//   return (
-//     <div className="student-login-page">
-//       <div className="student-login-container">
-//         <h1 className="login-heading">Student Login</h1>
-//         <form className="student-login-form" onSubmit={handleSubmit}>
-//           <label htmlFor="email">Email</label>
-//           <input
-//             type="email"
-//             id="email"
-//             value={email}
-//             onChange={(e) => setEmail(e.target.value)}
-//             required
-//           />
-
-//           <label htmlFor="password">Password</label>
-//           <div className="password-field">
-//             <input
-//               type={showPassword ? "text" : "password"}
-//               id="password"
-//               value={password}
-//               onChange={(e) => setPassword(e.target.value)}
-//               required
-//             />
-//             <button
-//               type="button"
-//               onClick={() => setShowPassword(!showPassword)}
-//               className="eye-icon"
-//             >
-//               {showPassword ? <FaEyeSlash /> : <FaEye />}
-//             </button>
-//           </div>
-
-//           <div className="error-messages">
-//             {errors.map((error, index) => (
-//               <p key={index} style={{ color: "red" }}>
-//                 {error}
-//               </p>
-//             ))}
-//           </div>
-
-//           <button type="submit" className="login-btn">
-//             Login
-//           </button>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default StudentLogin;
-
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, NavLink } from "react-router-dom";
 import "./StudentLogin.css";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
+import img from "../../assets/studentlogin.jpg";
 
 const StudentLogin = () => {
   const [email, setEmail] = useState("");
@@ -116,54 +35,71 @@ const StudentLogin = () => {
           navigate("/hostels");
         });
       } else {
-        r.json().then((e) => setErrors([e.error])); // Assuming error is a string
+        r.json().then((e) => setErrors([e.error])); //make the error messages to be an array
       }
     });
   }
 
   return (
-    <div className="student-login-page">
-      <div className="student-login-container">
-        <h1 className="login-heading">Student Login</h1>
+    <div className="student-login-page container">
+      <div className="student-login-container container">
+        <div className="student-login-image">
+          <img src={img} alt="student" />
+        </div>
         <form className="student-login-form" onSubmit={handleSubmit}>
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-
-          <label htmlFor="password">Password</label>
-          <div className="password-field">
+          <div className="student-login-form-title">
+            <h1 className="h2-heading">Welcome back!</h1>
+            <p>Login</p>
+          </div>
+          <div className="student-login-input">
+            <label htmlFor="email">Email</label>
             <input
-              type={showPassword ? "text" : "password"}
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               required
             />
-            <button
-              type="button"
-              onClick={() => setShowPassword(!showPassword)}
-              className="eye-icon"
-            >
-              {showPassword ? <FaEyeSlash /> : <FaEye />}
-            </button>
+          </div>
+          <div className="student-login-password">
+            <label htmlFor="password">Password</label>
+            <div className="student-login-password-field">
+              <input
+                type={showPassword ? "text" : "password"}
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className="student-login-eye-icon"
+              >
+                {showPassword ? <FaEyeSlash /> : <FaEye />}
+              </button>
+            </div>
+          </div>
+          <div className="student-forgot-pass">
+            <NavLink to="/forgot-password">Forgot password?</NavLink>
           </div>
 
           <div className="error-messages">
             {errors.map((error, index) => (
-              <p key={index} style={{ color: "red" }}>
-                {error}
-              </p>
+              <p key={index}>{error}</p>
             ))}
           </div>
 
-          <button type="submit" className="login-btn">
+          <button type="submit" className="student-login-btn">
             Login
           </button>
+
+          <div className="student-dont-have-account">
+            <p>
+              Don't have an account?
+              <NavLink to="/student-signup">Signup</NavLink>
+            </p>
+          </div>
         </form>
       </div>
     </div>
@@ -171,4 +107,3 @@ const StudentLogin = () => {
 };
 
 export default StudentLogin;
-
