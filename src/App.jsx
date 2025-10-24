9ed5417bed84a2657866a68f70182a1d4c10e5c0
mwendwamark <mwendwamark310@gmail.com>
2025-10-24 14:51:41 +0300

    add main tag to app.jsx and change the footer background color on small screen sizes

--- src/App.jsx
+++ src/App.jsx
@@ -26,46 +26,51 @@ import LandlordDashboard from "./Landlords/Dashboard/HomeDashboard/LandlordDashb
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/role-selection" element={<RoleSelection />} />
-        <Route path="/student/signup" element={<StudentSignup />} />
-        <Route path="/landlord/signup" element={<LandlordSignup />} />
-        <Route path="/student/login" element={<StudentLogin />} />
-        <Route path="/landlord/login" element={<LandlordLogin />} />
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/role-selection" element={<RoleSelection />} />
+          <Route path="/student/signup" element={<StudentSignup />} />
+          <Route path="/landlord/signup" element={<LandlordSignup />} />
+          <Route path="/student/login" element={<StudentLogin />} />
+          <Route path="/landlord/login" element={<LandlordLogin />} />
 
-        {/* Password Reset Routes */}
-        <Route
-          path="/student/forgot-password"
-          element={<PasswordResetRequest userType="student" />}
-        />
-        <Route
-          path="/landlord/forgot-password"
-          element={<PasswordResetRequest userType="landlord" />}
-        />
-        <Route
-          path="/student/password/edit"
-          element={<PasswordResetConfirm userType="student" />}
-        />
-        <Route
-          path="/landlord/password/edit"
-          element={<PasswordResetConfirm userType="landlord" />}
-        />
+          {/* Password Reset Routes */}
+          <Route
+            path="/student/forgot-password"
+            element={<PasswordResetRequest userType="student" />}
+          />
+          <Route
+            path="/landlord/forgot-password"
+            element={<PasswordResetRequest userType="landlord" />}
+          />
+          <Route
+            path="/student/password/edit"
+            element={<PasswordResetConfirm userType="student" />}
+          />
+          <Route
+            path="/landlord/password/edit"
+            element={<PasswordResetConfirm userType="landlord" />}
+          />
 
-        <Route path="/landlord/dashboard" element={<LandlordDashboard />} />
-        <Route path="/hostels" element={<HostelsPage />} />
-        <Route path="/landlord-profile" element={<LandlordProfile />} />
-        <Route path="/student-profile" element={<StudentProfile />} />
-        <Route path="/get-started" element={<GetStarted />} />
-        <Route path="/create-hostel" element={<CreateHostel />} />
-        <Route path="/hostels/:id" element={<HostelDetails />} />
-        <Route path="/write-review" element={<ReviewForm />} />
-        <Route path="/hostels/:hostelId/reviews" element={<ReviewsList />} />
-        <Route path="/wishlist" element={<Wishlist />} />
-        <Route path="/landlord/hostels/:id/update" element={<UpdateHostel />} />
-        <Route path="/landlord/my-hostels" element={<MyHostels />} />
-      </Routes>
+          <Route path="/landlord/dashboard" element={<LandlordDashboard />} />
+          <Route path="/hostels" element={<HostelsPage />} />
+          <Route path="/landlord-profile" element={<LandlordProfile />} />
+          <Route path="/student-profile" element={<StudentProfile />} />
+          <Route path="/get-started" element={<GetStarted />} />
+          <Route path="/create-hostel" element={<CreateHostel />} />
+          <Route path="/hostels/:id" element={<HostelDetails />} />
+          <Route path="/write-review" element={<ReviewForm />} />
+          <Route path="/hostels/:hostelId/reviews" element={<ReviewsList />} />
+          <Route path="/wishlist" element={<Wishlist />} />
+          <Route
+            path="/landlord/hostels/:id/update"
+            element={<UpdateHostel />}
+          />
+          <Route path="/landlord/my-hostels" element={<MyHostels />} />
+        </Routes>
+      </main>
     </>
   );
 }
